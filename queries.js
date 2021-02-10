dbfcd9e58368415f49bbac78382444da5e1f94dd
macht <macht@users.noreply.github.com>
2021-02-09 21:56:03 -0600

    Create additional endpoints

--- queries.js
+++ queries.js
@@ -26,7 +26,97 @@ const getCircuitById = (req, res) => {
     })
 }
 
+const getSeasons = (req, res) => {
+    pool.query('SELECT * FROM seasons ORDER BY year DESC', (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
+const getSeasonByYear = (req, res) => {
+    const id = parseInt(req.params.year)
+
+    pool.query('SELECT * FROM seasons WHERE year = $1', [year], (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
+const getConstructors = (req, res) => {
+    pool.query('SELECT * FROM constructors ORDER BY constructorid ASC', (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
+const getConstructorById = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query('SELECT * FROM constructors WHERE constructorid = $1', [id], (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
+const getConstructorByName = (req, res) => {
+    const name = req.params.name
+
+    pool.query('SELECT * FROM constructors WHERE name LIKE %$1%', [name], (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.status(200),json(result.rows)
+    })
+}
+
+const getDrivers = (req, res) => {
+    pool.query('SELECT * FROM drivers ORDER BY driverid ASC', (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
+const getDriverById = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query('SELECT * FROM drivers WHERE driverid = $1', [id], (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
+const getDriverByName = (req, res) => {
+    const name = req.params.name
+
+    pool.query('SELECT * FROM drivers WHERE forename LIKE %$1% OR surname LIKE %$1%', [name], (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.status(200),json(result.rows)
+    })
+}
+
 module.exports = {
     getCircuits,
-    getCircuitById
+    getCircuitById,
+    getSeasons,
+    getSeasonByYear,
+    getConstructors,
+    getConstructorById,
+    getConstructorByName,
+    getDrivers,
+    getDriverById,
+    getDriverByName
 }
