e0b23efeff1a563b51dbae9dae24a48b795a8371
macht <macht@users.noreply.github.com>
2021-03-25 00:00:07 -0500

    Add new endpoints for new stats pages

--- queries.js
+++ queries.js
@@ -46,6 +46,52 @@ const getCircuitsHot = (req, res) => {
     })
 }
 
+const getCircuitsBySeason = (req, res) => {
+    const year = parseInt(req.params.year)
+
+    pool.query('SELECT DISTINCT c.* FROM circuits c '
+    + 'INNER JOIN races rc ON c.circuitid = rc.circuitid AND rc.year = $1', [year], (err, result) => {
+        if (err) {
+            res.status(500).send('500 Internal Server Error')
+            return
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
+const getCircuitFastest = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query('SELECT rc.year, MIN(r.milliseconds) AS fastest FROM results r '
+    + 'INNER JOIN races rc ON r.raceid = rc.raceid '
+    + 'WHERE rc.circuitid = $1 '
+    + 'GROUP BY rc.year '
+    + 'ORDER BY rc.year', [id], (err, result) => {
+        if (err) {
+            res.status(500).send('500 Internal Server Error')
+            return
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
+const getCircuitFastestByDriver = (req, res) => {
+    const cid = parseInt(req.params.cid)
+    const did = parseInt(req.params.did)
+
+    pool.query('SELECT rc.year, MIN(r.milliseconds) AS fastest FROM results r '
+    + 'INNER JOIN races rc ON r.raceid = rc.raceid '
+    + 'WHERE rc.circuitid = $1 AND r.driverid = $2'
+    + 'GROUP BY rc.year '
+    + 'ORDER BY rc.year', [cid, did], (err, result) => {
+        if (err) {
+            res.status(500).send('500 Internal Server Error')
+            return
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
 const getSeasons = (req, res) => {
     pool.query('SELECT * FROM seasons ORDER BY year DESC', (err, result) => {
         if (err) {
@@ -70,6 +116,26 @@ const getSeasonByYear = (req, res) => {
 
 const getSeasonsHot = (req, res) => {
     pool.query('SELECT * FROM seasons ORDER BY year DESC LIMIT 3', (err, result) => {
+        if (err) {
+            res.status(500).send('500 Internal Server Error')
+            return
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
+const getSeasonWinner = (req, res) => {
+    const year = parseInt(req.params.year)
+
+    pool.query('SELECT d.forename || \' \' || d.surname AS winningdriver, c.name AS winningconstructor FROM results r '
+    + 'INNER JOIN races rc ON r.raceid = rc.raceid '
+    + 'LEFT OUTER JOIN drivers d ON r.driverid = d.driverid '
+    + 'LEFT OUTER JOIN constructors c ON r.constructorid = c.constructorid '
+    + 'WHERE rc.year = $1 '
+    + 'GROUP BY winningdriver, winningconstructor '
+    + 'HAVING SUM(points) > 0 '
+    + 'ORDER BY SUM(points) DESC '
+    + 'LIMIT 1', [year], (err, result) => {
         if (err) {
             res.status(500).send('500 Internal Server Error')
             return
@@ -124,6 +190,36 @@ const getConstructorsHot = (req, res) => {
     })
 }
 
+const getConstructorsBySeason = (req, res) => {
+    const year = parseInt(req.params.year)
+
+    pool.query('WITH seasonconstructors AS (SELECT DISTINCT constructorid FROM results r INNER JOIN races rc ON r.raceid = rc.raceid AND rc.year = $1) '
+    + 'SELECT DISTINCT c.* FROM constructors c '
+    + 'INNER JOIN seasonconstructors sc ON c.constructorid = sc.constructorid', [year], (err, result) => {
+        if (err) {
+            res.status(500).send('500 Internal Server Error')
+            return
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
+const getConstructorPoints = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query('SELECT rc.year, SUM(r.points) FROM results r '
+    + 'INNER JOIN races rc ON r.raceid = rc.raceid '
+    + 'WHERE r.constructorid = $1 '
+    + 'GROUP BY rc.year '
+    + 'ORDER BY rc.year', [id], (err, result) => {
+        if (err) {
+            res.status(500).send('500 Internal Server Error')
+            return
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
 const getDrivers = (req, res) => {
     pool.query('SELECT * FROM drivers ORDER BY driverid ASC', (err, result) => {
         if (err) {
@@ -170,6 +266,20 @@ const getDriversHot = (req, res) => {
     })
 }
 
+const getDriversBySeason = (req, res) => {
+    const year = parseInt(req.params.year)
+
+    pool.query('WITH seasondrivers AS (SELECT DISTINCT driverid FROM results r INNER JOIN races rc ON r.raceid = rc.raceid AND rc.year = $1) '
+    + 'SELECT DISTINCT d.* FROM drivers d '
+    + 'INNER JOIN seasondrivers sd ON d.driverid = sd.driverid', [year], (err, result) => {
+        if (err) {
+            res.status(500).send('500 Internal Server Error')
+            return
+        }
+        res.status(200).json(result.rows)
+    })
+}
+
 const getRaces = (req, res) => {
     pool.query('SELECT * FROM races ORDER BY raceid ASC', (err, result) => {
         if (err) {
@@ -183,7 +293,7 @@ const getRaces = (req, res) => {
 const getRacesByCircuit = (req, res) => {
     const id = parseInt(req.params.id)
 
-    pool.query('SELECT r.raceid, r.year, r.circuitid, r.name, r.date, d.forename || \' \' || d.surname as winner, re.time, c.name as constructor '
+    pool.query('SELECT r.raceid, r.year, r.circuitid, r.name, r.date, d.forename || \' \' || d.surname as winner, re.milliseconds as time, c.name as constructor '
     + 'FROM races r LEFT JOIN results re on r.raceid = re.raceid AND re.position = 1 '
     + 'LEFT JOIN drivers d on re.driverid = d.driverid '
     + 'LEFT JOIN constructors c on re.constructorid = c.constructorid '
@@ -199,7 +309,7 @@ const getRacesByCircuit = (req, res) => {
 const getRacesBySeason = (req, res) => {
     const year = parseInt(req.params.year)
 
-    pool.query('SELECT r.raceid, r.year, r.circuitid, r.name, r.date, d.forename || \' \' || d.surname as winner, re.time, c.name as constructor '
+    pool.query('SELECT r.raceid, r.year, r.circuitid, r.name, r.date, d.forename || \' \' || d.surname as winner, re.milliseconds as time, c.name as constructor '
     + 'FROM races r LEFT JOIN results re on r.raceid = re.raceid AND re.position = 1 '
     + 'LEFT JOIN drivers d on re.driverid = d.driverid '
     + 'LEFT JOIN constructors c on re.constructorid = c.constructorid '
@@ -216,17 +326,24 @@ module.exports = {
     getCircuits,
     getCircuitById,
     getCircuitsHot,
+    getCircuitsBySeason,
+    getCircuitFastest,
+    getCircuitFastestByDriver,
     getSeasons,
     getSeasonByYear,
     getSeasonsHot,
+    getSeasonWinner,
     getConstructors,
     getConstructorById,
     getConstructorByName,
     getConstructorsHot,
+    getConstructorsBySeason,
+    getConstructorPoints,
     getDrivers,
     getDriverById,
     getDriverByName,
     getDriversHot,
+    getDriversBySeason,
     getRaces,
     getRacesByCircuit,
     getRacesBySeason
